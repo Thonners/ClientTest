450e08590a437713f8634b261974fff76a49316e
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2016-12-26 23:29:21 +0000

    Add functionality to either send data to the server, or receive data from the server.
    - Send data via DataOutputStream, with leading bit dictating the type of connection and therefore the response expected.

--- src/clienttest/ClientTest.java
+++ src/clienttest/ClientTest.java
@@ -1,6 +1,7 @@
 package clienttest;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,25 +18,68 @@ public class ClientTest {
     
 //    private static String serverURL = "thonners.ddns.net" ; 
 //    private static int serverPort = 8080 ;
-    private static String serverURL = "localhost" ;
-    private static int serverPort = 9000 ;
+    private final static String serverURL = "localhost" ;
+    private final static int serverPort = 9000 ;
+    
+    private final static int SEND = 1 ;
+    private final static int RECEIVE = -1 ;
 
+    /**
+     * Main method - run when the project is executed. 
+     * Provided an integer is passed, will create an instance of this class and 
+     * either send or receive data to/from the server.
+     * @param args Command-line arguments passed - should be an in, either '1' or '-1', for a send or receive type client connection respectively.
+     */
     public static void main(String[] args) {
-        
-        System.out.println("Starting client...");      
-        
-        String serverMsg = "No message :(" ;
-        
+        // Try to parse the input, and create an instance if successful
+        int clientType = SEND ;
         try {
-            Socket s = new Socket(serverURL, serverPort);
-            
-            System.out.println("Socket conection created. Getting data...");
+            if (args.length == 0) {
+                System.out.println("Warning: no arguement given, so assuming SEND client type.");
+            } else {
+                // Parse the input
+                clientType = Integer.parseInt(args[0]) ;
+                
+                // Create a client instance to manage the rest of the show
+                ClientTest client = new ClientTest(clientType) ;
+            }
+        } catch (Exception e) {
+            System.out.println("Error parsing int. Illegal arguement given. Exiting.");    
+            return ;
+        }
+    }
+    
+    /**
+     * @param clientType 1 will start a 'send' client connection, to send data to the server, whilst '-1' will receive the data from the server.
+     */
+    public ClientTest(int clientType) {
+        // Create socket connection
+        switch(clientType) {
+            case SEND: 
+                initialiseSendClient() ;
+                break;
+            case RECEIVE: 
+                initialiseReceiveClient() ;
+                break ;
+            default :
+                
+        }
+        
+    }
+    
+    /**
+     * Creates a socket connection to the server and sends a series of messages.
+     * TODO: Include feedback from server as to whether the messages have been received without error.
+     */
+    private void initialiseSendClient() {
+        System.out.println("Starting send client...");     
+        try {
+            Socket s = new Socket(serverURL, serverPort);            
+            System.out.println("Socket conection created. Sending data...");
 
-            // Try fancy DataWriter to be able to distinguish between what's being sent
+            // DataOutputStream lets us be able to distinguish between what's being sent
             DataOutputStream dOut ;
             
-            PrintWriter outp = null;
-            BufferedReader inp = null;
             try {
                 dOut = new DataOutputStream(s.getOutputStream());
                 // Write the data
@@ -60,18 +104,74 @@ public class ClientTest {
                 dOut.writeByte(-1);
                 dOut.flush();
                 
-                //outp = new PrintWriter(s.getOutputStream(), true);
-                // Receive message from server
-                inp = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                String line = "" ;
-                while ((line = inp.readLine()) != null) {
-                    serverMsg += line + "\n" ;
-                }
-                
-                // Close the socket
+                // Close the stream & socket                
                 dOut.close() ;
+                s.close();
             } catch (IOException e) {
-                System.out.println("Error getting input/output stream:");
+                System.out.println("Error getting output stream:");
+                e.printStackTrace();
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error creating socket:");
+            e.printStackTrace() ;
+            return ;
+        }
+    }
+    
+    /**
+     * Creates a socket connection to the server to receive messages.
+     */
+    private void initialiseReceiveClient() {        
+        System.out.println("Starting receive client...");      
+        // Initialise String to hold server's message
+        String serverMsg = "No message from server :(" ;
+        
+        try {
+            Socket s = new Socket(serverURL, serverPort);            
+            System.out.println("Socket conection created. Sending data...");
+
+            // DataOutputStream is required so we can tell the server we want some info back
+            DataOutputStream dOut ;
+            // DattaInputStream will be used to read the data from the server
+            DataInputStream dIn ;
+            
+            try {
+                dOut = new DataOutputStream(s.getOutputStream());
+                dIn = new DataInputStream(s.getInputStream());
+                
+                // Tell the server we're expecting some data back
+                dOut.writeByte(RECEIVE) ;
+                dOut.flush() ;
+                
+                // Receive the response
+                
+                boolean done = false;
+                while(!done) {
+                    byte messageType = dIn.readByte();
+                    System.out.println("Byte from server: " + messageType);
+                    int messageCount = 0 ;
+                    serverMsg = "" ;
+                    switch(messageType) {
+                        case RECEIVE: // Data received from server
+                            String line = dIn.readUTF() ;
+                            serverMsg += line +"\n" ;
+                            System.out.println("String from server: " + line);
+                            messageCount++ ;
+                            done = true;
+                            break;
+                        default:
+                            done = true;
+                    }
+                    serverMsg += messageCount + " messages received from the server in this instance" ;
+                }
+
+                // Close the streams & socket
+                dOut.close() ;
+                dIn.close();
+                s.close();
+            } catch (IOException e) {
+                System.out.println("Error getting output stream:");
                 e.printStackTrace();
             }
             
@@ -81,10 +181,9 @@ public class ClientTest {
             return ;
         }
         
-       
-            System.out.println();
-            System.out.println();
-            System.out.println("Server message: " + serverMsg);
-       
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Complete message from server: " + serverMsg);
     }
 }
