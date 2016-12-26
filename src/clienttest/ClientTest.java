efbd5198f8efa2f1deadc7fcf326fa47c3b58947
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2016-12-26 21:35:05 +0000

    Update Input/OutputStreams to use DataOutputStream, which lets us define what type of message will be sent. Send three messages to the server, then receive a message back.

--- src/clienttest/ClientTest.java
+++ src/clienttest/ClientTest.java
@@ -1,6 +1,7 @@
 package clienttest;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -15,7 +16,9 @@ import java.net.Socket;
 public class ClientTest {
     
 //    private static String serverURL = "thonners.ddns.net" ; 
+//    private static int serverPort = 8080 ;
     private static String serverURL = "localhost" ;
+    private static int serverPort = 9000 ;
 
     public static void main(String[] args) {
         
@@ -24,19 +27,49 @@ public class ClientTest {
         String serverMsg = "No message :(" ;
         
         try {
-            Socket s = new Socket(serverURL, 9000);
+            Socket s = new Socket(serverURL, serverPort);
             
             System.out.println("Socket conection created. Getting data...");
 
+            // Try fancy DataWriter to be able to distinguish between what's being sent
+            DataOutputStream dOut ;
+            
             PrintWriter outp = null;
             BufferedReader inp = null;
             try {
-                outp = new PrintWriter(s.getOutputStream(), true);
+                dOut = new DataOutputStream(s.getOutputStream());
+                // Write the data
+                
+                // Send first message
+                dOut.writeByte(1);
+                dOut.writeUTF("First Message.");
+                dOut.flush(); // Send off the data
+                
+                // Send second message
+                dOut.writeByte(2);
+                dOut.writeUTF("Second Message.");
+                dOut.flush(); // Send off the data
+                
+                // Send third message
+                dOut.writeByte(3);
+                dOut.writeUTF("Third , and ");
+                dOut.writeUTF("final message.");
+                dOut.flush(); // Send off the data
+                
+                // Stop it listening for more
+                dOut.writeByte(-1);
+                dOut.flush();
+                
+                //outp = new PrintWriter(s.getOutputStream(), true);
+                // Receive message from server
                 inp = new BufferedReader(new InputStreamReader(s.getInputStream()));
                 String line = "" ;
                 while ((line = inp.readLine()) != null) {
                     serverMsg += line + "\n" ;
                 }
+                
+                // Close the socket
+                dOut.close() ;
             } catch (IOException e) {
                 System.out.println("Error getting input/output stream:");
                 e.printStackTrace();
